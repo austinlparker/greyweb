3e1ce7f37184dd6202b14cbb41d04927ebe9e8de
serewit <root@serewit.com>
2014-02-05 14:18:54 -0500

    updates and stuff

--- js/grayscale.js
+++ js/grayscale.js
@@ -20,8 +20,8 @@ $(function() {
 
 //Google Map Skin - Get more at http://snazzymaps.com/
 var myOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(53.385873, -1.471471),
+    zoom: 7,
+    center: new google.maps.LatLng(42.6862, -73.8239),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
     styles: [{
